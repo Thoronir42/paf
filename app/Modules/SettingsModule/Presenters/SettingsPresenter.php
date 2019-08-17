d72eceef3c46d994aff29bf107d819cf14c44743
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-08-17 23:24:26 +0200

    Cleanup SettingsPresenter

--- app/Modules/SettingsModule/Presenters/SettingsPresenter.php
+++ app/Modules/SettingsModule/Presenters/SettingsPresenter.php
@@ -2,11 +2,7 @@
 
 namespace PAF\Modules\SettingsModule\Presenters;
 
-use Exception;
-use Nette\Application\AbortException;
 use PAF\Common\BasePresenter;
-use PAF\Modules\SettingsModule\Components\SettingsControl\OptionNodeControl;
-use PAF\Modules\SettingsModule\InlineOption\SettingsOptionAccessor;
 use SeStep\GeneralSettings\Model\INode;
 
 final class SettingsPresenter extends BasePresenter
@@ -14,8 +10,6 @@ final class SettingsPresenter extends BasePresenter
     public function startup()
     {
         parent::startup();
-
-//        $this->validateAuthorization('admin-settings', Authorizator::READ, ':Common:Homepage:');
     }
 
     /**
