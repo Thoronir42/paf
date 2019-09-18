f8cc32fb0b9dda06c0394abdd43dea026677df71
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-09-19 00:21:28 +0200

    Minor cleanup around FileAttachable

---
+++ extensions/SeStep/FileAttachable/database/initialize.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `ss_files__user_file_thread`
+(
+    id           INT      NOT NULL AUTO_INCREMENT,
+    date_created DATETIME NULL,
+
+    CONSTRAINT `ss_f_user_file_thread_pk` PRIMARY KEY (id)
+)
