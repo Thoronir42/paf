3c408ca1d33355897929e30bae07b4a1ebdb1f2b
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-11-10 13:26:10 +0100

    Cleanup Commission module extension and services

--- app/Modules/CommissionModule/CommissionModuleExtension.php
+++ app/Modules/CommissionModule/CommissionModuleExtension.php
@@ -3,31 +3,11 @@
 namespace PAF\Modules\CommissionModule;
 
 use Nette\DI\CompilerExtension;
-use Nette\DI\ContainerBuilder;
-use PAF\Common\Workflow\WorkflowUtils;
-use PAF\Modules\CommissionModule\Model\PafCaseWorkflow;
-use Symfony\Component\Workflow\Definition;
-use Symfony\Component\Workflow\MarkingStore\MethodMarkingStore;
 
 class CommissionModuleExtension extends CompilerExtension
 {
     public function loadConfiguration()
     {
-        $builder = $this->getContainerBuilder();
-
-        $this->registerPafCaseWorkflowDefinition($builder);
-    }
-
-    private function registerPafCaseWorkflowDefinition(ContainerBuilder $builder)
-    {
-        $caseWorkflowDefinition = $builder->addDefinition($this->prefix('pafCaseWorkflowDefinition'))
-            ->setType(Definition::class)
-            ->setArgument('places', PafCaseWorkflow::getStates())
-            ->setArgument('transitions', PafCaseWorkflow::getTransitions())
-            ->setArgument('initialPlaces', [PafCaseWorkflow::STATUS_ACCEPTED])
-            ->setAutowired(false);
-
-        $builder->addDefinition($this->prefix('pafCaseWorkflow'))
-            ->setType(PafCaseWorkflow::class);
+        $this->loadDefinitionsFromConfig($this->loadFromFile(__DIR__ . '/commissionModule.neon'));
     }
 }
