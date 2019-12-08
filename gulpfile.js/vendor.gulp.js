8ab6f35a14ff7971f83618c66b090be3b5acdfe3
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-12-08 15:47:35 +0100

    Restore xeditable functionality for BS4 .. sort of

--- gulpfile.js/vendor.gulp.js
+++ gulpfile.js/vendor.gulp.js
@@ -6,11 +6,13 @@ module.exports.copyVendor = async () => {
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'node_modules/select2/dist/js/select2.full.min.js',
+            'node_modules/X-editable/dist/bootstrap3-editable/js/bootstrap-editable.js',
         ],
         'css': [
             'node_modules/font-awesome/css/font-awesome.min.css',
             'node_modules/select2/dist/css/select2.min.css',
             'node_modules/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.css',
+            'node_modules/X-editable/dist/bootstrap3-editable/css/bootstrap-editable.css',
         ],
         'fonts': [
             'node_modules/font-awesome/fonts/*'
---
+++ www/js/xEditable-bs4-polyfill.js
@@ -0,0 +1,9 @@
+$(function () {
+    $.fn.editableform.buttons =
+        '<button type="submit" class="btn btn-primary btn-sm editable-submit">' +
+        '  <i class="fa fa-check"></i>' +
+        '</button>' +
+        '<button type="button" class="btn btn-secondary btn-sm editable-cancel">' +
+        '  <i class="fa fa-remove"></i>' +
+        '</button>';
+});
