fd4f20e217beb8ccd0b3e268bf4f48b5758207b5
Štěpán Śevčík <Thoronir42@Gmail.com>
2019-12-12 14:01:28 +0100

    Unify feed control appearance by sharing latte ancestor

--- app/Common/Feed/Components/FeedControl/FeedEntryControl.php
+++ app/Common/Feed/Components/FeedControl/FeedEntryControl.php
@@ -3,6 +3,8 @@
 namespace PAF\Common\Feed\Components\FeedControl;
 
 use Nette\Application\UI\Control;
+use Nette\Application\UI\ITemplate;
+use Nette\Bridges\ApplicationLatte\Template;
 use PAF\Common\Feed\FeedEvents;
 
 abstract class FeedEntryControl extends Control
@@ -14,6 +16,14 @@ abstract class FeedEntryControl extends Control
     public function __construct(FeedEvents $events)
     {
         $this->events = $events;
+    }
+
+    protected function createTemplate(): ITemplate
+    {
+        $template = parent::createTemplate();
+        $template->layout = __DIR__ . '/@feedEntry.latte';
+
+        return $template;
     }
 
     /**
