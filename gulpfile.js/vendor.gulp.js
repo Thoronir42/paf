f6171f4c6201535a46e291f87ca54a0772f54e1c
Štěpán Śevčík <Thoronir42@Gmail.com>
2020-01-03 11:12:52 +0100

    Polish structure of frontend dependencies

--- gulpfile.js/vendor.gulp.js
+++ gulpfile.js/vendor.gulp.js
@@ -1,35 +1,60 @@
 const gulp = require('gulp');
 
+const dependencies = [
+    {
+        src: 'node_modules/jquery/dist/*',
+        dst: 'jQuery',
+    },
+    {
+        src: [
+            'node_modules/font-awesome/css/*',
+            'node_modules/font-awesome/fonts/*',
+        ],
+        srcOptions: {
+            base: 'node_modules/font-awesome',
+        },
+        dst: 'font-awesome',
+    },
+    {
+        src: 'node_modules/moment/min/**',
+        dst: 'moment',
+    },
+    {
+        src: 'node_modules/bootstrap/dist/**/*',
+        dst: 'bootstrap',
+    },
+    {
+        src: 'node_modules/bs-custom-file-input/dist/*',
+        dst: 'bootstrap/bs-custom-file-input',
+    },
+    {
+        src: 'node_modules/X-editable/dist/bootstrap3-editable/**/*',
+        dst: 'bootstrap/x-editable',
+    },
+    {
+        src: 'node_modules/tempusdominus-bootstrap-4/build/**/*',
+        dst: 'bootstrap/tempusdominus'
+    },
+    {
+        src: 'node_modules/select2/dist/**/*',
+        dst: 'select2',
+    },
+    {
+        src: 'node_modules/@ttskch/select2-bootstrap4-theme/dist/*',
+        dst: 'select2/bootstrap4-theme',
+    },
+];
+
+
 module.exports.copyVendor = async () => {
-    const vendorFolders = {
-        'js': [
-            'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/bs-custom-file-input/dist/bs-custom-file-input.js',
-            'node_modules/select2/dist/js/select2.full.min.js',
-            'node_modules/X-editable/dist/bootstrap3-editable/js/bootstrap-editable.js',
-            'node_modules/moment/min/locales.min.js',
-            'node_modules/moment/min/moment.min.js',
-            'node_modules/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.js',
-        ],
-        'css': [
-            'node_modules/font-awesome/css/font-awesome.min.css',
-            'node_modules/select2/dist/css/select2.min.css',
-            'node_modules/@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.css',
-            'node_modules/X-editable/dist/bootstrap3-editable/css/bootstrap-editable.css',
-            'node_modules/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css',
-        ],
-        'fonts': [
-            'node_modules/font-awesome/fonts/*'
-        ],
-    };
-
     const folderTasks = [];
-    for (let folder in vendorFolders) {
-        const sources = vendorFolders[folder];
 
-        folderTasks.push(gulp.src(sources).pipe(gulp.dest('www/vendor/' + folder)));
-    }
+    dependencies.forEach((dependency) => {
+        let task = gulp.src(dependency.src, dependency.srcOptions)
+            .pipe(gulp.dest('www/vendor/' + dependency.dst));
+
+        folderTasks.push(task);
+    });
 
     await Promise.all(folderTasks);
 };
