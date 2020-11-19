f6b1c68bcc450c64a0211790bc33ad2352d5ac55
Štěpán Śevčík <Thoronir42@Gmail.com>
2020-11-19 17:40:10 +0100

    Add common router module

---
+++ app/Modules/CommonModule/CommonRouterModule.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace PAF\Modules\CommonModule;
+
+use Nette\Application\Routers\RouteList;
+use PAF\Common\Router\RouterModule;
+
+class CommonRouterModule implements RouterModule
+{
+    public function setRoutes(RouteList $routeList): void
+    {
+        $routeList->addRoute('sign-in', 'Sign:in');
+    }
+}
